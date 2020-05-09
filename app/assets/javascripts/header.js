eff15ae843bd8431ed8347872223f2c5ad5787ca
hoang000147 <dithocsinh@gmail.com>
2020-05-09 14:32:56 +0700

    update html, scss, javascript sidebar

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -33,10 +33,7 @@ $(document).on('turbolinks:load', function(){
       $("#user-image").hide()
       $("#user-avatar").show()
     })
-})
 
-// toggle
-$(document).ready(function () {
   $('#sidebarCollapse').on('click', function () {
       $('.sidebar').toggleClass('active');
       var temp = document.getElementById("sidebarCollapse");
@@ -45,4 +42,17 @@ $(document).ready(function () {
       else temp.textContent = "Show Rooms";
       //temp.getElementById("span").textContent="Show Room";
   });
-});
+})
+
+// toggle
+/*$(document).ready(function () {
+  $('#sidebarCollapse').on('click', function () {
+      $('.sidebar').toggleClass('active');
+      var temp = document.getElementById("sidebarCollapse");
+      if (temp.textContent == "Show Rooms")
+        temp.textContent = "Hide Rooms";
+      else temp.textContent = "Show Rooms";
+      //temp.getElementById("span").textContent="Show Room";
+  });
+});*/
+
